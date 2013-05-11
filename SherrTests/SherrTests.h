e397a16beada44aac4b0f8c292b2fbd50f3a90a6
sassembla <toru.inoue@kissaki.tv>
2013-05-11 12:40:10 +0900

    Initial Commit

---
+++ SherrTests/SherrTests.h
@@ -0,0 +1,13 @@
+//
+//  SherrTests.h
+//  SherrTests
+//
+//  Created by sassembla on 2013/05/11.
+//  Copyright (c) 2013年 KISSAKI Inc,. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SherrTests : SenTestCase
+
+@end
---
+++ SherrTests/SherrTests.m
@@ -0,0 +1,32 @@
+//
+//  SherrTests.m
+//  SherrTests
+//
+//  Created by sassembla on 2013/05/11.
+//  Copyright (c) 2013年 KISSAKI Inc,. All rights reserved.
+//
+
+#import "SherrTests.h"
+
+@implementation SherrTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SherrTests");
+}
+
+@end
